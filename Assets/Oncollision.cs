7471870df8139f96e5437b1bfac6e7f741b6abf4
TheLamboMilk <alexandru.tichilesteanu@gmail.com>
2021-11-16 21:07:37 +0200

    OH NOOOOOOOOOO, Oncollision works

--- Assets/Oncollision.cs
+++ Assets/Oncollision.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class Oncollision : MonoBehaviour
 {
-   
+     void OnCollisionEnter2D(Collision2D other) 
+     {
+       Debug.Log("Mono:NUUUUUUUUUUUU, masinuta s-a spart");
+     }
 }
