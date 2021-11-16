2b8880921a74f0247f9c47c6857bf12f18148eab
TheLamboMilk <alexandru.tichilesteanu@gmail.com>
2021-11-16 21:30:03 +0200

    Changing some things in code

--- Assets/Oncollision.cs
+++ Assets/Oncollision.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class Oncollision : MonoBehaviour
 {
-     void OnCollisionEnter2D(Collision2D other) 
-     {
-       Debug.Log("Mono:NUUUUUUUUUUUU, masinuta s-a spart");
-     }
-     
-     void OnTriggerEnter2D(Collider2D other) 
-     {
-       Debug.Log("Mono:A inceput Cars 3");
-     }
+   private void OnCollisionEnter2D(Collision2D other) {
+     Debug.Log("Mono:NUUUUUUUUUUUUU, masinuta s-a spart");
+   }
+   private void OnTriggerEnter2D(Collider2D other) {
+     Debug.Log("Mono:A inceput cars 3");
+   }
 }
