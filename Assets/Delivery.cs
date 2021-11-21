9400f865580a6c5540e8cb4954b73212943f38b8
TheLamboMilk <alexandru.tichilesteanu@gmail.com>
2021-11-21 21:25:26 +0200

    packagecolor

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 { 
+  [SerializeField] Color32 hasPackageColor = new Color32 (1,1,1,1);
+  [SerializeField] Color32 noPackageColor = new Color32 (1,1,1,1);
   [SerializeField] float destroyDelay = 0.5f;
   bool hasPackage;
+
+  SpriteRenderer spriteRenderer;
+
+  void Start()
+  {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
 
     void OnCollisionEnter2D(Collision2D other) 
    {
@@ -17,12 +26,14 @@ public class Delivery : MonoBehaviour
      {
        Debug.Log("Filmul cu cars 3 a fost ridicat");
        hasPackage = true;
+       spriteRenderer.color = hasPackageColor;
        Destroy(other.gameObject,destroyDelay);
      }
      if (other.tag == "Mono" && hasPackage)
      {
        Debug.Log("Filmul cu cars 3 a fost livrat domnului Mono");
        hasPackage = false;
+       spriteRenderer.color = noPackageColor;
      }
    }
 }
