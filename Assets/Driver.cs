6c7ebb41c99e60192dc3e5cd413af3120192ac9d
TheLamboMilk <alexandru.tichilesteanu@gmail.com>
2021-11-21 22:03:43 +0200

    Adding boost and slowness

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float delay = 0.5f;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0,0, -steerAmount);
         transform.Translate(0,moveAmount,0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Boost")
+        {
+            Debug.Log("S-a dus la goana!");
+            moveSpeed = boostSpeed;
+            Destroy(other.gameObject,delay);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = slowSpeed;
     }
 }
